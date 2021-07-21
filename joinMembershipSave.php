586d1015b3136d2b65925e92dd0c0853755c668f
kimzzeong <kimmju12@naver.com>
2021-07-21 15:11:06 +0000

    commit

--- joinMembershipSave.php
+++ joinMembershipSave.php
@@ -2,15 +2,23 @@
   include "db.php";
   if(isset($_POST['user_email']) && isset($_POST['user_password']) && isset($_POST['user_nickname'])){    
 
-    $query = mq("SELECT * FROM user");
+    $query = mq("SELECT * FROM user where user_email = '".$email."'");
+    $user =  $query->fetch_array();
 
     $password = password_hash($_POST['user_password'], PASSWORD_BCRYPT );
-   //$password = password_hash($_POST['user_password'],PASSWORD_DEFAULT);
 
-    $sql = mq("INSERT INTO user
-    (user_email,user_password,user_nickname)
-    VALUES('{$_POST['user_email']}','{$password}','{$_POST['user_nickname']}')");
+    if(isset($user['user_email'])){
+      $response['status'] = "fail";
   
-  echo $query;
+    }else{
+
+      $response['status'] = "success";
+
+      $sql = mq("INSERT INTO user
+      (user_email,user_password,user_nickname)
+      VALUES('{$_POST['user_email']}','{$password}','{$_POST['user_nickname']}')");
+    }
+  
+  echo json_encode($response);
   }
 ?>
--- test.php
+++ test.php
@@ -1,4 +1,8 @@
 <?php
-
-
+    include "db.php";
+    $email = "111";
+    $pass = "111";
+    $query = mq("SELECT * FROM user where user_email = '".$email."'");
+    $test =  $query->fetch_array();
+    echo $test['user_nickname'];
 ?>
