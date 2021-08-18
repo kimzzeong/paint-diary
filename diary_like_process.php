930a9c5173a68a81ed5f22199a5504ece03b2001
kimzzeong <kimmju12@naver.com>
2021-08-18 03:58:53 +0000

    diary_info like commit

--- diary_like_process.php
+++ diary_like_process.php
@@ -16,12 +16,15 @@ if(isset($_GET['user_idx']) && isset($_GET['diary_idx'])){
             $like_status = 1;
         }
         $update = mq("update user_diary_like set like_status = '".$like_status."' where user_idx = '".$user_idx."' and diary_idx = '".$diary_idx."'");
-
+        $reponse['message'] = "성공";
     }else{
         $insert = mq("INSERT INTO user_diary_like
         (diary_idx,user_idx,like_status)
         VALUES('{$diary_idx}','{$user_idx}','{$like_status}')");
+        $reponse['message'] = "실패";
     }
+
+    echo json_encode($reponse);
 
 }
 
