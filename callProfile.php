26086c0bcc9b662bdd0f614909a0cab6faa6d5f8
kimzzeong <kimmju12@naver.com>
2021-09-14 06:31:09 +0000

    lock screen commit

--- callProfile.php
+++ callProfile.php
@@ -4,10 +4,12 @@
         $user_idx = $_POST['user_idx'];
         $query = mq("SELECT * FROM user where user_idx = '".$user_idx."'");
         $user =  $query->fetch_array();
+        $sql = mq("select * from diary where diary_status = 0 and diary_writer = '".$_POST['user_idx']."' order by diary_date desc, diary_idx desc");
     
         $response['user_nickname'] = $user['user_nickname'];
         $response['user_introduction'] = $user['user_introduction'];
         $response['user_profile'] = $user['user_profile'];
+        $response['user_diary_count'] = mysqli_num_rows($sql);
 
         echo json_encode($response);
 
