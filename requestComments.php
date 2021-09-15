acc2c12ac99f5e36661f6aa4b14ccc76e9c270dc
kimzzeong <kimmju12@naver.com>
2021-09-15 11:33:33 +0000

    calendar commit

--- requestComments.php
+++ requestComments.php
@@ -2,6 +2,8 @@
 include $_SERVER['DOCUMENT_ROOT']."/db.php"; /* db load */
 
 $sql = mq("select * from diary_comments where diary_idx = '".$_POST['diary_idx']."' and comment_status = 0 order by comment_datetime");
+$sql_diary_writer = mq("select * from diary where diary_idx = '".$_POST['diary_idx']."'");
+$diary_writer=$sql_diary_writer->fetch_array(); //sql2의 결과값에서 닉네임만 변수에 넣기
 $response = array();
 while($row = mysqli_fetch_assoc($sql)){
     $sql_user = mq("select * from user where user_idx='".$row['comment_writer']."'"); /* 받아온 idx값을 선택 */
--- test.php
+++ test.php
@@ -1,36 +1,7 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT']."/db.php"; /* db load */
 
-$user_password = "dxGp8Gdl";
-$user_email = "kimmju12@naver.com";
-
-$sql_hash_pw = mq("select * from user where user_email='".$user_email."'");
-$user = $sql_hash_pw->fetch_array();
-$hash_pw = $user['user_password'];
-
-$sql_password=mq("SELECT * FROM user where user_email='$user_email' && user_password = '$hash_pw'");
 
 
-
-if($result=mysqli_fetch_array($sql_password)){//쿼리문을 실행해서 결과가 있으면 로그인 성공
-    $sql_useridx=mq("SELECT * FROM user where user_email='".$user_email."'");
-    $user = $sql_useridx->fetch_array();
-    if($user['user_status'] == 1){
-      $response['user_idx'] = null;
-      $response['user_nickname'] = null;
-      $response['status'] = 'null';
-    }else{
-      if (password_verify($user_password, $hash_pw)) {
-          $response['user_idx'] = $user['user_idx'];
-          $response['user_nickname'] = $user['user_nickname'];
-          $response['status'] = 'success';
-      }else{
-        $response['user_idx'] = null;
-        $response['user_nickname'] = null;
-        $response['status'] = 'fail';
-      }
-    }
-  }
- // echo $
-  print_r($response);
+echo $diary_writer['diary_writer'];
 ?>
